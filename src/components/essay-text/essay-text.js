803da813b5ceaa4ee0ed4f8f387786de7719e1ee
akinsola <thomas.lawanson@gmail.com>
2020-12-02 23:31:36 +0000

    update

---
+++ src/components/essay-text/essay-text.js
@@ -0,0 +1,48 @@
+import React from "react"
+import styled from "styled-components";
+
+const EssayTextWrapper = styled.div`
+    padding: 1rem;
+`
+
+
+const EssayText = props => {
+  return (
+    <EssayTextWrapper>
+        <p>
+          {" "}
+          I'm baby pollinator salvia gentrify, tempor anim qui readymade kale chips
+          DIY. Adipisicing tofu narwhal distillery nulla leggings four loko
+          glossier 90's authentic fanny pack cardigan you probably
+          haven't heard of them pinterest. Master cleanse fingerstache
+          distillery adipisicing. Neutra messenger bag pitchfork narwhal. Retro
+          yuccie cronut pork belly. Selvage sunt magna, kale chips af disrupt
+          fingerstache nostrud. +1 banjo non, live-edge meggings trust fund
+          locavore single-origin coffee cupidatat nisi duis selvage biodiesel do
+          pok pok. Trust fund jianbing qui, kickstarter farm-to-table skateboard
+          you probably haven't heard of them. Ut officia 90's knausgaard
+          gentrify coloring book anim. Ugh lo-fi green juice kitsch lorem plaid.
+          Fixie pour-over kitsch VHS pitchfork unicorn. Brunch shaman YOLO,
+          pinterest VHS live-edge next level labore twee selvage venmo. +1 raw
+          denim jianbing, disrupt kogi nostrud fanny pack. Blog synth shoreditch
+          incididunt. Tumblr neutra ea intelligentsia, lorem butcher incididunt
+          man braid. Ugh adaptogen knausgaard quis, vinyl activated charcoal
+          consequat palo santo ea reprehenderit. Asymmetrical fashion axe
+          sustainable fugiat lomo vaporware irony fixie raw denim artisan pop-up
+          8-bit retro lo-fi aliqua. Tbh hella marfa artisan, retro culpa 8-bit
+          helvetica deep v everyday carry. Ut try-hard single-origin coffee,
+          direct trade nulla mustache vinyl photo booth. Laborum VHS DIY
+          farm-to-table, gochujang sed vinyl velit pug listicle. Before they
+          sold out jianbing in activated charcoal proident, church-key butcher
+          flexitarian. Ut blue bottle narwhal meggings slow-carb. Sed try-hard
+          chambray af. Vaporware pabst semiotics synth leggings exercitation
+          lo-fi voluptate trust fund sriracha qui pour-over. Raw denim echo park
+          prism chambray salvia, drinking vinegar fingerstache trust fund. Next
+          level paleo ex retro ennui truffaut schlitz, in disrupt craft beer
+          tumblr.
+        </p>
+      </EssayTextWrapper>
+  )
+}
+
+export default EssayText;
